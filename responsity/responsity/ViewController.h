c4a124fbc85ba6a618b0f0ec86be4300a3695414
徐丽 <xuli5@le.com>
2017-02-14 10:41:56 +0800

    no message

---
+++ responsity/responsity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  responsity
+//
+//  Created by 徐丽 on 17/2/14.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ responsity/responsity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  responsity
+//
+//  Created by 徐丽 on 17/2/14.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
